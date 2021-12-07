dafa2fe72390d14d4747a801b5a00ac59413509a
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-12-07 15:10:23 +0530

    Update settings.js

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
-        api : "http://localhost:3001",
-        //api : "https://forgot-password123.herokuapp.com",
+        //api : "http://localhost:3001",
+        api : "https://shopnode123.herokuapp.com",
         secret:"rzp_test_rFkV7RYeBZOzCE"
 
     }
